1c331973a02c472af8582a1f68cece281dd31585
Dima-kzk <dimakzk@yahoo.com>
2023-09-09 14:46:34 +0300

    step 4 done

---
+++ src/components/App/App.jsx
@@ -0,0 +1,61 @@
+import { Component } from 'react';
+import AddContactForm from '../AddContactForm/AddContactForm';
+import { Сentralizer } from './App.styled';
+import { ContactsList } from 'components/ContactsList/ContactsList';
+// import { nanoid } from 'nanoid';
+import Section from 'components/Section';
+import { Filter } from 'components/Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  AddContact = (name, number, id) => {
+    // this.state.contacts.push({ name, id });
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { name, id, number }],
+    }));
+  };
+
+  InputHandle = event => {
+    if (event.currentTarget.name === 'filter') {
+      this.setState({ filter: event.currentTarget.value });
+    }
+  };
+
+  filterByName = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+    return (
+      <Сentralizer>
+        <Section title="Phonebook">
+          <AddContactForm
+            AddContact={this.AddContact}
+            state={this.state}
+            InputChange={this.InputHandle}
+            // onSubmit={this.AddContact}
+          />
+        </Section>
+        <Section title="Contacts">
+          <Filter state={this.state} InputChange={this.InputHandle} />
+          <ContactsList contacts={filter ? this.filterByName() : contacts} />
+        </Section>
+      </Сentralizer>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,11 @@
+export function ContactsList({ contacts }) {
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id}>
+          {contact.name}: {contact.number}
+        </li>
+      ))}
+    </ul>
+  );
+}
